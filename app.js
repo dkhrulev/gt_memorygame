da7f463b202561e2150433855459bebe5cf7b22f
Dmitrii K <smartman3099@gmail.com>
2024-12-03 23:31:51 -0600

    Photo js & randomomizer

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ const playerLives = 10;
 
 //link text
 playerLivesCount.textContent = playerLives;
+
+//Generate the data
+// [{imgSrc: './images/Clemens.jpg'}]
+
+const getData = () => [
+    {imgSrc: './images/Clemens.jpg', name: 'Mr. Clemens'},
+    {imgSrc: './images/Hodek.jpg', name: 'Ms. Houdek'},
+    {imgSrc: './images/Medina.jpg', name: 'Ms. Medina'},
+    {imgSrc: './images/Clemens.jpg', name: 'Mr. Clemens'},
+    {imgSrc: './images/Hodek.jpg', name: 'Ms. Houdek'},
+    {imgSrc: './images/Medina.jpg', name: 'Ms. Medina'},
+    {imgSrc: './images/Abby.jpg', name: 'Coach Abby'},
+    {imgSrc: './images/Mallernee.jpg', name: 'Coach Mallernee'},
+    {imgSrc: './images/Rodriguez.jpg', name: 'Coach Rodriguez'},
+    {imgSrc: './images/Abby.jpg', name: 'Coach Abby'},
+    {imgSrc: './images/Mallernee.jpg', name: 'Coach Mallernee'},
+    {imgSrc: './images/Rodriguez.jpg', name: 'Coach Rodriguez'},
+    
+];
+
+//const data = getData();
+//RANDOMIZE
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5)
+    console.log(cardData)
+};
+
+randomize()
