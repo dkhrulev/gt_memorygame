242281a603d751ed2980edf3243c92ac6ffbe19c
Dmitrii K <smartman3099@gmail.com>
2024-12-03 22:00:10 -0600

    add lives

---
+++ app.js
@@ -0,0 +1,7 @@
+const section = document.querySelector('section');
+
+const playerLivesCount = document.querySelector('span');
+const playerLives = 10;
+
+//link text
+playerLivesCount.textContent = playerLives;
